aed3dd148ccc8d053f029d31830b4629032d4fe1
haroon-developerr <haroon51166@gmail.com>
2025-04-26 02:06:23 +0400

    Added Workflow For Backend

--- API-jokes/app.js
+++ API-jokes/app.js
@@ -33,7 +33,7 @@ app.get('/api/getJokes',async (req,res)=>{
 })
 
 app.get('/api',(req,res)=>{
-   res.json({message:"application is up and running v1"})
+   res.json({message:"application is up and running v2"})
 })
 
 app.post('/api/post-joke',async (req,res)=>{
