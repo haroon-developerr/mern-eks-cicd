3296121ce84a5b3e30a8e173c901423525d4ba1d
haroon-developerr <haroon51166@gmail.com>
2025-04-26 02:12:37 +0400

    backend testing again

--- API-jokes/app.js
+++ API-jokes/app.js
@@ -33,7 +33,7 @@ app.get('/api/getJokes',async (req,res)=>{
 })
 
 app.get('/api',(req,res)=>{
-   res.json({message:"application is up and running v3"})
+   res.json({message:"application is up and running v4"})
 })
 
 app.post('/api/post-joke',async (req,res)=>{
